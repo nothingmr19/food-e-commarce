9a7e92c4d7765047471813129e059df5eb8ed390
Mahmudul Hasan <rakibahmadmr@gmail.com>
2024-04-16 01:58:22 +0600

    done

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,34 @@
+import { Fragment } from "react";
+import { Navbar } from "@/components/global";
+import { Footer } from "@/components/global/footer";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <Fragment>
+      <Navbar />
+      <div className="py-[96px]">
+        <div className="w-full lg:max-w-[480px] mx-auto">
+          <Image src="/error.png" alt="" height={388} width={480} />
+          <div className="text-center">
+            <h4 className="h4 pb-4">Page not found!</h4>
+            <p className="h6 font-normal text-gray-600 mb-6">
+              We&apos;re sorry! This page is currently unavailable. We request
+              you to please try again later.
+            </p>
+          </div>
+          <Link href="/">
+            <button
+              type="submit"
+              className="py-3 px-4 w-full flex justify-center items-center gap-2 rounded-lg border font-semibold bg-[#28AD1F] text-white hover:bg-[#208B18]"
+            >
+              Back to Home
+            </button>
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </Fragment>
   );
 }
