95bcc3ce9dcd02345e2fb113cec49c9aefdd641f
Mahmudul Hasan <rakibahmadmr@gmail.com>
2024-04-16 21:49:26 +0600

    error fix

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -4,8 +4,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Image from "next/image";
-import banner1 from "../../assest/banner/banner1.png";
-import banner3 from "../../assest/banner/banner3.png";
+import banner1 from "@/assest/banner/banner1.png";
+import banner3 from "@/assest/banner/Banner3.png";
 
 export function BannerPage() {
   var settings = {
