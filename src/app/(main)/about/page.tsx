e7353da1845057f141cac61b442ed3ff084e28b2
rahadsr30 <rahadahmedsr30@gmail.com>
2024-04-16 21:12:54 +0600

    all done

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -23,11 +23,11 @@ export default function page() {
             <li>
               <a href="#" className="h6">
                 {" "}
-                Contact us
+                About us
               </a>
             </li>
           </ol>
-          <h3 className="h4 text-center mt-2">Contact us</h3>
+          <h3 className="h4 text-center mt-2">About us</h3>
         </nav>
       </div>
       <div className="mx-auto max-w-4xl py-[96px]">
@@ -43,7 +43,7 @@ export default function page() {
             freshness, and nutritional value in every bite.
           </h6>
         </div>
-        <div className="">
+        <div className="mt-16">
           <h4 className="text-[24px] font-semibold leading-[28px] text-gray-900">
             Our Story:
           </h4>
@@ -58,7 +58,7 @@ export default function page() {
             passionate food enthusiasts dedicated to bringing this vision to
             life.
           </h6>
-          <Image src="" alt="" width={896} height={374} />
+          <Image src="/about.png" alt="" width={896} height={374} />
           <h6 className="h6 pt-4 font-normal">
             [Food Company Name] was founded with a simple yet powerful idea: to
             create food products that inspire and delight. Our journey began in
@@ -71,7 +71,7 @@ export default function page() {
             life.
           </h6>
         </div>
-        <div className="">
+        <div className="mt-16">
           <h4 className="text-[24px] font-semibold leading-[28px] text-gray-900">
             Our Mission:
           </h4>
@@ -79,7 +79,42 @@ export default function page() {
             At [Food Company Name], our mission is to enrich lives through
             exceptional food experiences. We strive to achieve this by:
           </h6>
-          <ol>
+          <ol className="list-decimal ml-6 text-gray-600 mt-5 h6 font-normal">
+            <li>
+              Crafting Quality Products: We meticulously select the finest
+              ingredients and adhere to rigorous quality standards to ensure
+              that every product bearing our name meets or exceeds our customers
+              expectations.
+            </li>
+            <li>
+              Innovating with Flavors: We continuously push the boundaries of
+              culinary innovation, exploring new flavor profiles, and culinary
+              techniques to create products that surprise and delight the
+              senses.
+            </li>
+            <li>
+              Promoting Healthy Eating: We are committed to promoting health and
+              wellness through our products by offering nutritious options made
+              with wholesome ingredients and mindful preparation methods.
+            </li>
+            <li>
+              Sustainability and Responsibility: We believe in conducting our
+              business in an environmentally and socially responsible manner.
+              From sourcing sustainable ingredients to reducing our carbon
+              footprint, we are dedicated to making a positive impact on the
+              planet and our communities.
+            </li>
+          </ol>
+        </div>
+        <div className="mt-16">
+          <h4 className="text-[24px] font-semibold leading-[28px] text-gray-900">
+            Our visson:
+          </h4>
+          <h6 className="h6 pt-4 font-normal">
+            At [Food Company Name], our mission is to enrich lives through
+            exceptional food experiences. We strive to achieve this by:
+          </h6>
+          <ol className="list-decimal ml-6 text-gray-600 mt-5 h6 font-normal">
             <li>
               Crafting Quality Products: We meticulously select the finest
               ingredients and adhere to rigorous quality standards to ensure
